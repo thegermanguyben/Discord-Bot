5b5ae2bece1020f940174ebbaa144aa488e2494e
Ben <bencredo@icloud.com>
2024-01-27 12:08:28 +0100

    event handler added

--- index.js
+++ index.js
@@ -26,31 +26,17 @@ for (const folder of commandFolders) {
     }
 }
 
-client.on(Events.InteractionCreate, async interaction => {
-    if (!interaction.isChatInputCommand()) return;
-    const command = interaction.client.commands.get(interaction.commandName);
+const eventsPath = path.join(__dirname, 'events');
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith('.js'));
 
-    if(!command) {
-        console.error(`NO COMMAND ${interaction.commandName}`);
-        return;
+for (const file of eventFiles) {
+    const filePath = path.join(eventsPath, file);
+    const event = require(filePath);
+    if (event.once) {
+        client.once(event.name, (...args) => event.execute(...args));
+    } else {
+        client.on(event.name, (...args) => event.execute(...args));
     }
+}
 
-    try {
-        await command.execute(interaction);
-    } catch (error) {
-        console.error(error);
-        if (interaction.replied || interaction.deferred) {
-            await interaction.followUp({ content: 'Error', ephemeral: true});
-        } else {
-            await interaction.reply({ content: 'Error', ephemeral: true});
-        }
-    }
-})
-client.once(Events.ClientReady, readyClient => {
-    console.log(`Up as ${readyClient.user.tag}`);
-});
-client.on(Events.InteractionCreate, interaction => {
-    if (!interaction.isChatInputCommand()) return;
-    console.log(interaction);
-})
 client.login(process.env.TOKEN)
